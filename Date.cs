e22d4380f82554b9e965a244ede4005f939ee340
Cristian Muñoz <cristiand.munozt@outlook.com>
2023-07-04 19:48:07 -0500

    Add project files.

---
+++ Date.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConceptos
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date()
+        {
+            _year = 1990;
+            _month = 1;
+            _day = 1;
+        }
+        public Date(int year, int month, int day)
+        {
+            _year = ValidateYear(year);
+            _month = ValidateMonth(month);
+            _day = ValidateDay(day);
+        }
+
+       
+
+        public int Yaer {
+            get => _year;
+            set => _year = ValidateYear(value); 
+        }
+        public int Month {
+            get => _month;
+            set => _month =ValidateMonth(value);
+        }
+        public int Day {
+            get => _day;
+            set => _day =ValidateDay(value);
+        }
+
+       
+        public override string ToString()
+        {
+            return $"{Yaer, 4:0000}/{Month, 2:00}/{Day, 2:00}";
+        }
+
+        private int ValidateYear(int year)
+        {
+            if (year < 0)
+            {
+                throw new ArgumentException("Año no valido");
+            }
+            return year ;
+        }
+
+        private int ValidateMonth(int month)
+        {
+            if(month < 1 || month > 12)
+            {
+                throw new ArgumentException("Mes no valido");
+            }
+            return month ;
+
+        }
+        private int ValidateDay(int day)
+        {
+            if (day == 29 && _month == 2 && IsLeapYear(_year))
+            {
+                return day ;
+            }
+            if ((day >= 1 && day <= 28 && Month == 2)|| 
+                (day >= 1 && day <= 30 && Month == 4) || (day >= 1 && day <= 30 && Month == 6) ||
+                (day >= 1 && day <= 30 && Month == 9) || (day >= 1 && day <= 30 && Month == 11) ||
+                (day >= 1 && day <= 31 && Month == 1) || (day >= 1 && day <= 31 && Month == 3) ||
+                (day >= 1 && day <= 31 && Month == 5 ) || (day >= 1 && day <= 31 && Month == 7) ||
+                (day >= 1 && day <= 31 && Month == 8) || (day >= 1 && day <= 31 && Month == 10) ||
+                (day >= 1 && day <= 31 && Month == 12)
+                )
+            {
+                return day;
+            }
+
+            throw new ArgumentException("Dia no valido");
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//// Tipos de datos
+//int myInt = 5;
+//double myDouble = 3.434;
+//float myFloat = 4.56f;
+//decimal myDecimal = 4.56m;
+
+//string myString = "Cristian";
+
+//bool myBool = true;
+
+//var myVar = true;
+
+
+//Console.WriteLine($"Mi valor entero es.: {myInt,20:N2}"); // String format
+//Console.WriteLine($"Mi valor doble es..: {myDouble, 20:N2}");
+//Console.WriteLine($"Mi valor float es..: {myFloat, 20:P2}");
+//Console.WriteLine($"Mi valor decimal es: {myDecimal,20:C2}");
+//Console.WriteLine($"Mi nombres es......: {myString}");
+//Console.WriteLine($"Mi bolleano es.....: {(myBool ? "Correcto": "incorrecto")}");  // if en una sola linea ternarios
+
+
+
+using OOPConceptos;
+try
+{
+    var fecha1 = new Date()
+    {
+        Yaer = 2023,
+        Month = 2,
+        Day = 28,
+    };
+    Console.WriteLine(fecha1);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+Console.WriteLine("Sigo en pie ..!");
+
+
+
+
+
+
+
+
+
+
+
+
+
